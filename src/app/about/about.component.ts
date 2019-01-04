bdc082753d20bae3eec718d1b70d11025e506068
Nabendu <nabendu.biswas@gmail.com>
2019-01-04 06:53:15 +0530

    DemoAngular

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  users:Object;
+  
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.fromData().subscribe(data => {
+      this.users = data
+      console.log(this.users);
+    }
+  );
+  }
+
+}
